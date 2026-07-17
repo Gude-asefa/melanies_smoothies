c4b16bd94ca66526bf85125537c55d744e0bdaf2
GUDINA ASEFA <agudinaasefa@gmail.com>
2026-07-17 17:12:45 +0300

    Enhance smoothie app with API data integration
    Added requests and pandas imports, and modified ingredient string construction to include API response data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests  
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothies :cup_with_straw:")
@@ -42,7 +44,10 @@ if options :
     ingredients_string = ''
 
     for fruits_chosen in options:
-        ingredients_string += fruits_chosen
+      ingredients_string += fruits_chosen
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      
 
     st.write(ingredients_string)
 
@@ -55,11 +60,7 @@ if options :
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-import requests  
-import pandas as pd
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
+
  
 #st.text(smoothiefroot_response)
 #smoothiefroot_json = smoothiefroot_response.json()
