bc04833a6d48980bcc2025a4dde6bfaccd42e955
GUDINA ASEFA <agudinaasefa@gmail.com>
2026-07-17 17:26:33 +0300

    Refactor dataframe handling in streamlit_app.py
    Updated dataframe handling to convert to pandas before displaying. Removed old dataframe display code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,12 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+
+pd_df= my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 
@@ -46,6 +51,10 @@ if options :
 
     for fruits_chosen in options:
       ingredients_string += fruits_chosen
+
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
       st.subheader(fruits_chosen + 'Nutrition Information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
       st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
